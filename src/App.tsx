6ddb11d7e43255be4eb116ee48bbae9015016ea6
Maxim-87 <maksboks87@mail.ru>
2022-12-06 07:45:57 +0600

    refactor App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
-import {addTodo, decrement, increment, removeLastTodo} from "./redux/mainReducer";
+import {decrement, increment, addTodo, removeLastTodo} from "./toolkit/toolkitSlice";
+
+
 
 function App() {
 
-    const {count, todos} = useSelector((state: any) => state.main)
+    const {count, todos} = useSelector((state: any) => state.toolkitSlice)
     const dispatch = useDispatch();
+
+    console.log(todos)
 
     const incrementHandler = () => {
         dispatch(increment())
@@ -17,37 +20,46 @@ function App() {
         dispatch(decrement())
     }
 
-   const addTodoHandler = () => {
-        dispatch(addTodo())
+    const addTodoHandler = () => {
+        dispatch(addTodo('new todo'))
     }
 
-   const removeTodoHandler = () => {
+    const removeTodoHandler = () => {
         dispatch(removeLastTodo())
     }
 
+    const addDispatchTodo = () => {
+        setTimeout(() => {
+            dispatch(addTodo('dispatch todo'));
+        }, 2000)
+    }
 
-  return (
-    <div className="App">
-        <div>
-            {count}
+
+    return (
+        <div className="App">
+            <div>
+                {count}
+            </div>
+            <button onClick={incrementHandler}>
+                Increment
+            </button>
+            <button onClick={decrementHandler}>
+                Decrement
+            </button>
+            <button onClick={addTodoHandler}>
+                Add todos
+            </button>
+            <button onClick={removeTodoHandler}>
+                Delete todos
+            </button>
+            <button onClick={addDispatchTodo}>
+                New todos
+            </button>
+            <ul>
+                {todos.map((todo: any) => <li> {todo} </li>)}
+            </ul>
         </div>
-     <button onClick={incrementHandler}>
-         Increment
-     </button>
-        <button onClick={decrementHandler}>
-         Decrement
-     </button>
-        <button onClick={addTodoHandler}>
-         Add todos
-     </button>
-        <button onClick={removeTodoHandler}>
-         Delete todos
-     </button>
-        <ul>
-            {todos.map((todo: any) => <li key={todo}> todo </li>)}
-        </ul>
-    </div>
-  );
+    );
 }
 
 export default App;
